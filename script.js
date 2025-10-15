e589b55ac060d95f0d9669c5ba2077758fe0729c
Shubham <shubham.dzimark@gmail.com>
2025-10-15 14:19:29 +0530

    Upto testimonial

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const swiper = new Swiper("#testimonials-slider", {
+        pagination: {
+            el: "#testimonials-pagination",
+            clickable: true,
+        },
+    })
+})
